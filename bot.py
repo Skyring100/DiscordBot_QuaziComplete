a681f641b020b564ec96ce7d4d124c4e945820fe
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-12-03 23:44:38 -0800

    Added a spam command and changed spacing

--- bot.py
+++ bot.py
@@ -3,18 +3,23 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
+#startup
 load_dotenv(".env")
 TOKEN: str = os.getenv("TOKEN")
-
 client = commands.Bot(command_prefix="Q.", intents=discord.Intents.all())
-
 @client.event
 async def on_ready():
     await client.tree.sync()
     print("Quazi Clone online")
 
+#commands
 @client.tree.command(name="hello_world", description="Say hello to my little friend!")
 async def hello_world(interaction: discord.Interaction):
     await interaction.response.send_message("Hello World!")
 
+@client.tree.command(name="spam", description="Spam a message")
+async def spam(interaction: discord.Interaction, message: str, amount: int = 5):
+    await interaction.response.send_message("What have you done", ephemeral=True)
+    for i in range(amount):
+        await interaction.channel.send(message)
 client.run(TOKEN)
