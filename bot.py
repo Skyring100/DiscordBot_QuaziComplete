6ebf1be19743f999b7493a0eeafad00388423093
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-12-04 19:20:45 -0800

    Added led controlling functionality

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import pin_functions
 
 #startup
 load_dotenv(".env")
@@ -22,4 +23,10 @@ async def spam(interaction: discord.Interaction, message: str, amount: int = 5):
     await interaction.response.send_message("What have you done", ephemeral=True)
     for i in range(amount):
         await interaction.channel.send(message)
+
+@client.tree.command(name="change_led", description="Changes LED on hardware")
+async def change_led(interaction: discord.Interaction, isOn: bool):
+    pin_functions.change_led(isOn)
+    await interaction.response.send_message("LED changed")
+
 client.run(TOKEN)
---
+++ pin_functions.py
@@ -0,0 +1,7 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+led_pin = 16
+GPIO.setmode(led_pin, GPIO.OUT)
+
+def change_led(isOn: bool):
+    GPIO.output(led_pin, isOn)
