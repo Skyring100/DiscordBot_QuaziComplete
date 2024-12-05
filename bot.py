6b82b7ce2eb8696b785d6df43feec8c96d4393ef
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-12-04 20:05:17 -0800

    Gitignore ignores Python's cache

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+import dotenv
 import os
 
 bot_has_pin_commands: bool = False
@@ -11,7 +11,7 @@ except ModuleNotFoundError:
     print("Bot not run on Raspberry Pi, skipping pin functions")
 
 #startup
-load_dotenv(".env")
+dotenv.load_dotenv(".env")
 TOKEN: str = os.getenv("TOKEN")
 client = commands.Bot(command_prefix="Q.", intents=discord.Intents.all())
 
