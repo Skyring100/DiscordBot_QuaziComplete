b01ce06d179b651067b3f84b0e6e306c389bfe1a
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-12-04 23:00:46 -0800

    Added basic audio commands for joining a voice channel. Also working on youtube player

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import dotenv
 import os
+import yt_dlp
+from discord import FFmpegPCMAudio
+
 
 bot_has_pin_commands: bool = False
 try:
@@ -31,12 +34,50 @@ async def spam(interaction: discord.Interaction, message: str, amount: int = 5):
     for i in range(amount):
         await interaction.channel.send(message)
 
+#audio commands
+@client.tree.command(name="join_vc", description="Bot will join a voice channel")
+async def join_vc(interaction: discord.Interaction, voice_channel: discord.VoiceChannel):
+    await interaction.response.send_message("Joining voice channel", ephemeral=True)
+    return await voice_channel.connect()
+    
+@client.tree.command(name="vc_with_me", description="Bot will join the voice channel the user is in")
+async def vc_with_me(interaction: discord.Interaction):
+    await interaction.response.send_message("Joining voice channel", ephemeral=True)
+    return await interaction.user.voice.channel.connect()
+
+@client.tree.command(name="youtube", description="Play YouTube audio")
+async def youtube(interaction: discord.Interaction, url: str):
+    voice = interaction.guild.voice_client
+    if voice == None:
+        return await interaction.response.send_message("Bot needs to be in a voice channel for this")
+    #await clearYoutube()
+    if voice.is_playing():
+        return await interaction.response.send_message("Please wait until audio is finished")
+    video_name = await downloadVideo(url)
+    #voice.play(FFmpegPCMAudio(video_name))
+    await interaction.response.send_message("Download success")
+        
+#hardware commands
 @client.tree.command(name="change_led", description="Changes LED on hardware")
 async def change_led(interaction: discord.Interaction, is_on: bool):
     if bot_has_pin_commands:
         pin_functions.change_led(is_on)
-        await interaction.response.send_message("LED changed")
+        await interaction.response.send_message("LED changed", ephemeral=True)
     else:
         await interaction.response.send_message("Bot currently does not have access to pin I/O")
 
 client.run(TOKEN)
+
+#helper functions
+async def downloadVideo(url: str):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }], 'noplaylist': True
+    }
+    downloader = yt_dlp.YoutubeDL(ydl_opts)
+    downloader.download([url])
+    print(downloader.extract_info(url))
