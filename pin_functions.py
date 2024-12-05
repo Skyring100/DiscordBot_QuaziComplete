41ac75cfdab7b08e90b2bd1dbbc420c1e5f62860
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-12-04 20:08:30 -0800

    Fixed incorrect pin setup method used

--- pin_functions.py
+++ pin_functions.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 led_pin = 16
-GPIO.setmode(led_pin, GPIO.OUT)
+GPIO.setup(led_pin, GPIO.OUT)
 
 def change_led(is_on: bool):
     GPIO.output(led_pin, is_on)
