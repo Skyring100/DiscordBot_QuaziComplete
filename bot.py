e43e73814ab5815bfae0061086e1522a6c7b121d
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-12-06 12:27:59 -0800

    Response messages for quote of the day should not be ephemeral

--- bot.py
+++ bot.py
@@ -53,14 +53,14 @@ async def spam(interaction: discord.Interaction, message: str, amount: int = 5):
 
 @client.tree.command(name="quote_of_the_day", description="Selects a quote to be quote of the day!")
 async def quote_of_the_day(interaction: discord.Interaction):
-    await interaction.response.defer(ephemeral=True)
+    await interaction.response.defer()
     #check if database base quote data
     chosen_quote = db_cursor.execute("SELECT quotes.content, quotes.day_timestamp FROM quotes WHERE quotes.guild_id="+str(interaction.guild_id)).fetchone()
     if not chosen_quote:
         #this server has never used the 'quote of the day' command
         chosen_quote = await choose_random_quote(interaction.guild)
         if not chosen_quote:
-            await interaction.followup.send("There is no 'quotes' channel with quotes in it. Make one and start adding!", ephemeral=True)
+            await interaction.followup.send("There is no 'quotes' channel with quotes in it. Make one and start adding!")
             return
         #add the quote for this server to the database
         try:
@@ -76,7 +76,7 @@ async def quote_of_the_day(interaction: discord.Interaction):
             #we need to update quote of the day
             chosen_quote = await choose_random_quote(interaction.guild)
             if not chosen_quote:
-                await interaction.followup.send("There is no longer a quote in the quotes channel for this server", ephemeral=True)
+                await interaction.followup.send("There is no longer a quote in the quotes channel for this server")
                 return
             #update with the new quote
             try:
